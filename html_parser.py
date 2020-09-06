7a21e5d528834d8149fbc1f2b84ebbd5ec25d021
abc <abc@example.com>
2020-09-06 17:12:20 +0300

    mechanicalsoup

--- html_parser.py
+++ html_parser.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import mechanicalsoup
 
 url = "http://olympus.realpython.org/profiles/dionysus"
 
@@ -10,3 +11,23 @@ soup = BeautifulSoup(html, "html.parser")  #Beautifulsoup object arguments strin
 print(soup.get_text()) #gets the text and remove all the html tags
 
 soup.find_all("img") #returns all the img html_tags from the BeautifulSoup obj. in a list 
+
+
+browser = mechanicalsoup.Browser() #headless browser
+
+url = "http://olympus.realpython.org/login"
+login_page = browser.get(url) #gets the url
+login_html = login_page.soup #parses the html
+
+form = login_html.select("form")[0] #select all forms in the html
+form.select("input")[0]["value"] = "zeus"
+form.select("input")[1]["value"] = "ThunderDude"
+
+profile_page = browser.submit(form, login_page.url)
+
+links = profile_page.soup.select("a") #selects links with <a> ancor element in profile page html
+
+for link in links:
+    address = url + link["href"]
+    text = link.text 
+    print(f"{text}:{address}")
